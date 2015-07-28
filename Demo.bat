cf673595363bc1267e94313f2e119097873bb7b9
Shidel <shidel@users.noreply.github.com>
2015-07-28 08:05:17 -0400

    vcursor and batch fun

--- Demo.bat
+++ Demo.bat
@@ -1,10 +1,23 @@
 @echo off
 
-if not exist BIN call mkDemo.bat
+if not exist BIN\NUL goto MakeDemo
+goto DoDemo
+
+:MakeDemo
+if not exist mkDemo.bat goto NoJoy
+call mkDemo.bat
+goto DoDemo
+
+:NoJoy
+echo Unable to do demo.
+goto :Done
+
+:DoDemo
+if not exist BIN\VCURSOR.COM goto NoJoy
 
 cd bin
 
-rem redirect does not work in dos box
+rem redirect does not work in DosBox, so preset it to small
 set CURSOR=small
 vcursor | set /p CURSOR=
 
@@ -22,3 +35,4 @@ vcls /a 0x07
 vcursor %CURSOR%
 set CURSOR=
 cd ..
+:Done
