a764e6df0306897e03ac08c6369cf55756d9a78b
Shidel <shidel@users.noreply.github.com>
2015-09-01 06:46:26 -0400

    restructure demos

--- DEMOS/Demo.bat
+++ DEMOS/Demo.bat
@@ -3,7 +3,8 @@ rem Please note, the only reason this demo uses very poor and
 rem inconsistent command formating, is demonstrate the flexibility
 rem of the V8Power Tools option parsing.
 
-
+if exist DEMOS\NUL cd DEMOS
+if exist ..\BIN\VCLS.COM goto LetsGo
 if exist ..\VCLS.COM goto LetsGo
 
 :MissingTools
@@ -12,9 +13,8 @@ echo You may need to build them using the mkV8.bat script.
 goto Done
 
 :LetsGo
-goto Done
-
-rem cd bin
+cd ..
+if exist BIN\VCLS.COM cd BIN
 
 rem DosBOX Test
 set DBTEST=yes
@@ -239,23 +239,225 @@ vdelay 2000
 goto NextPart
 
 :PartB
-cd ..
-call Demo1.bat
-cd bin
-goto NextPart
+vcls /b Red /f White
+vdelay 500
+
+vcls /b Green /f LightCyan
+vdelay 500
+vcls /b Blue /f Yellow
+vgotoxy /x20 /y20
+vecho /n "You have just experienced a blue screen. " 
+vdelay 3000
+vecho /fGray "(" /fWhite "Of Life!" /fgray ')'
+vdelay 2000
+vframe /bGray /fBlack /x10/y5/w60/h8 Single
+vecho /fRed "There is a box on it!"
+vdelay 2000
+vecho /fBlue "You can write text to it!"
+vdelay 2000
+vecho /n /fBlue "and more text!"
+vdelay 1000
+vecho /fRed "and more!"
+vdelay 500
+vecho /fRed "and more!"
+vdelay 500
+vecho /fRed "and more!"
+vdelay 500
+vecho /fBrown "(Oops, I ran out of room. So, I had to scroll a little.)" /fred "and more!"
+vdelay 1000
+vecho /n /fRed "and more!"
+vdelay 1000
+vgotoxy /x 30 /y 15
+vecho /fRed /bWhite " Or not "
+vdelay  2000
+vecho "The text goes where you want it to!"
+vdelay  1000
+vecho "With very little effort."
+vdelay  2000
+
+vcls /b red
+vframe /bBlue /fCyan /x20/y10/w40/h6 Double
+vecho "This box has double sides box."
+vdelay  3000
+vframe /bBlue /fYellow /x20/y10/w40/h6 SingleSides
+vecho "This box has single sides and double top and bottom edges."
+vdelay  3000
+vframe /bBlue /fWhite /x20/y10/w40/h6 DoubleSides
+vecho "This box has double sides and single top and bottom edges."
+vdelay  3000
+vframe /bBlue /fLightGreen /x20/y10/w40/h6 Hidden
+vecho "This hidden edge box. Great for making text areas with margins."
+vdelay 3000
+vcls /l
+vdelay 2000
+vecho "That was a local clear screen."
+vdelay 2000
+vcls /g
+vdelay 1000
+vecho "That was a global clear screen."
+vdelay 2000
+
+vcls /bblue
+vframe /bGray /fWhite /x10/y5/w60/h15 Double
+vecho "This is another box."
+vdelay 1000
+vgotoxy	/l /x 3 /y 3
+vecho "This is Local position 3x3."
+vdelay 1000
+vgotoxy	/g /x 3 /y 3
+vecho "This is global position 3x3."
+vdelay 3000
+
+vcls /bblue
+vframe /bGray /fWhite /x15/y1/w10/h4 Double
+vframe /bGray /fWhite /x5/y7/w20/h5 Double
+vgotoxy /l /x1/y2
+vecho "Box characters cause interesting things with the vecho command"
+vgotoxy /g/x1/y1
+vecho "Box characters cause interesting things with the vecho command"
+vdelay 3000
+
+vcls /b Green /f LightCyan
+vecho "This screen is environmentally friendly."
+vdelay 2000goto NextPart
 
 :PartC
-cd ..
-call Demo2.bat
-cd bin
+vcursor Full
+vcls /bblue /fwhite
+
+vframe /bGray /fBlack /x5/y5/w70/h15 Single
+
+vcls /l
+vgotoxy /x25/y13
+vecho /n "First move to start of line."
+vgotoxy /l sol
+vdelay 2000
+
+vcls /l
+vgotoxy /x25/y13
+vecho /n "Now move to end of line."
+vgotoxy /l eol
+vdelay 2000
+
+vcls /l
+vgotoxy /x25/y13
+vecho /n "Next move to start of row."
+vgotoxy /l sor
+vdelay 2000
+
+vcls /l
+vgotoxy /x25/y13
+vecho /n "Now move to end of row."
+vgotoxy /l eor
+vdelay 2000
+
+vcls /l
+vgotoxy /x25/y13
+vecho "Now move to start of page."
+vgotoxy /l sop
+vdelay 2000
+
+vcls /l 
+vgotoxy /x25/y13
+vecho "Now move to end of page."
+vgotoxy /l eop
+vdelay 2000
+
+vcls /l
+vgotoxy /x25/y12
+vecho "Now move to start of text"
+vgotoxy /x25/y14
+vecho "(then -1)."
+vgotoxy /l sot previous
+vdelay 2000
+
+vcls /l
+vgotoxy /x25/y12
+vecho "Now move to end of text"
+vgotoxy /x25/y14
+vecho "(then +1)."
+vgotoxy /l eot next
+vdelay 2000
+
+vcls /l
+vgotoxy /x15/y13
+vecho /n "Finally, Lets Just move the cursor around a bit."
+vdelay 1000
+vgotoxy /l left
+vdelay 200
+vgotoxy /l left
+vdelay 200
+vgotoxy /l left
+vdelay 200
+vgotoxy /l left
+vdelay 200
+vgotoxy /l left
+vdelay 200
+vgotoxy /l left
+vdelay 200
+vgotoxy /l left
+vdelay 200
+vgotoxy /l left
+vdelay 200
+
+vgotoxy /l up
+vdelay 200
+vgotoxy /l up
+vdelay 200
+vgotoxy /l up
+vdelay 200
+vgotoxy /l up
+vdelay 200
+vgotoxy /l up
+vdelay 200
+vgotoxy /l up
+vdelay 200
+
+vgotoxy /l right
+vdelay 200
+vgotoxy /l right
+vdelay 200
+vgotoxy /l right
+vdelay 200
+vgotoxy /l right
+vdelay 200
+vgotoxy /l right
+vdelay 200
+vgotoxy /l right
+vdelay 200
+vgotoxy /l right
+vdelay 200
+vgotoxy /l right
+vdelay 200
+
+vgotoxy /l down
+vdelay 200
+vgotoxy /l down
+vdelay 200
+vgotoxy /l down
+vdelay 200
+vgotoxy /l down
+vdelay 200
+vgotoxy /l down
+vdelay 200
+vgotoxy /l down
+vdelay 200
+
+vdelay 1000
+
+vcls /l
+vgotoxy /x25/y13
+vecho "All done."
+vdelay 2000
+vcursor hide
 goto NextPart
 
 :NextPart
 goto StartUp
 
 :AllDone
-vcls text
-vdelay 2000
+vcls /bBlue
+vdelay 1000
 
 vcls 	/a0x07
 vecho Goodbye...
@@ -263,5 +465,6 @@ rem restore the cursor size and shape
 vcursor %MYCURSOR%
 set MYCURSOR=
 set DOSBOX_TEST=
-rem cd ..
 :Done
+if not exist DEMOS cd ..
+if exist DEMOS cd DEMOS
---
+++ EXAMPLES/README.txt
@@ -0,0 +1,11 @@
+*******************************************************************************
+V8Power Tools for DOS examples
+Copyright 2015 Jerome Shidel
+Released under GPL v2.0 License.
+*******************************************************************************
+
+This Directory contains some basic techniques and examples for using the
+V8Power Tools.
+
+The V8Power Tools must be present in your PATH environment variable to use
+these demos.
