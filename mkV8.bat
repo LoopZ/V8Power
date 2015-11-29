8e2595a84ec0c87ffa13880e2b4d7748b5e87502
Shidel <shidel@users.noreply.github.com>
2015-11-29 11:27:06 -0500

    dev

--- mkV8.bat
+++ mkV8.bat
@@ -23,143 +23,143 @@ if not "%1" == "" goto Done
 
 :vframe
 set VSTAGE=vframe
-bin\vecho /g "Examining " /f lightred coolant /f Gray ...
+bin\vecho /g/s- "Examining " /f lightred coolant /f Gray ...
 NASM.EXE -ISOURCE\ SOURCE\VFRAME.ASM -fbin -O9 -o BIN\VFRAME.COM
 if ERRORLEVEL 1 goto Error
 if not "%1" == "" goto Done
 
 :verrlvl
 set VSTAGE=vdelay
-bin\vecho /g "Studying " /f 10 "dipstick" /f gray ...
+bin\vecho /g/s- "Studying " /f 10 "dipstick" /f gray ...
 NASM.EXE -ISOURCE\ SOURCE\VERRLVL.ASM -fbin -O9 -o BIN\VERRLVL.COM
 if ERRORLEVEL 1 goto Error
 if not "%1" == "" goto Done
 
 :vgotoxy
 set VSTAGE=vgotoxy
-bin\vecho /g "Checking " /f yellow spark-plugs /f Gray ...
+bin\vecho /g/s- "Checking " /f yellow spark-plugs /f Gray ...
 NASM.EXE -ISOURCE\ SOURCE\VGOTOXY.ASM -fbin -O9 -o BIN\VGOTOXY.COM
 if ERRORLEVEL 1 goto Error
 if not "%1" == "" goto Done
 
 :vdelay
 set VSTAGE=vdelay
-bin\vecho /g "Cleaning " /f 15 "air filter" /f gray ...
+bin\vecho /g/s- "Cleaning " /f 15 "air filter" /f gray ...
 NASM.EXE -ISOURCE\ SOURCE\VDELAY.ASM -fbin -O9 -o BIN\VDELAY.COM
 if ERRORLEVEL 1 goto Error
 if not "%1" == "" goto Done
 
 :vmath
 set VSTAGE=vmath
-bin\vecho /g /n "Studying "
-bin\vecho /g /f 13 belts /f gray ...
+bin\vecho /g/s- /n "Studying "
+bin\vecho /g/s- /f 13 belts /f gray ...
 NASM.EXE -ISOURCE\ SOURCE\VMATH.ASM -fbin -O9 -o BIN\VMATH.COM
 if ERRORLEVEL 1 goto Error
 if not "%1" == "" goto Done
 
 :vstr
 set VSTAGE=vstr
-bin\vecho /g "Turning " /f 10 "the key" /f gray ...
+bin\vecho /g/s- "Turning " /f 10 "the key" /f gray ...
 NASM.EXE -ISOURCE\ SOURCE\VSTR.ASM -fbin -O9 -o BIN\VSTR.COM
 if ERRORLEVEL 1 goto Error
 if not "%1" == "" goto Done
 
 :vchoice
 set VSTAGE=vchoice
-bin\vecho /g "Verifying " /f 12 "timing" /f gray ...
+bin\vecho /g/s- "Verifying " /f 12 "timing" /f gray ...
 NASM.EXE -ISOURCE\ SOURCE\VCHOICE.ASM -fbin -O9 -o BIN\VCHOICE.COM
 if not "%1" == "" goto Done
 
 :vcursor
 set VSTAGE=vcursor
-bin\vecho /g /n "Adjusting "
-bin\vecho /g /f 13 carburetor /f gray ...
+bin\vecho /g/s- /n "Adjusting "
+bin\vecho /g/s- /f 13 carburetor /f gray ...
 NASM.EXE -ISOURCE\ SOURCE\VCURSOR.ASM -fbin -O9 -o BIN\VCURSOR.COM
 if ERRORLEVEL 1 goto Error
 if not "%1" == "" goto Done
 
 :vprogres
 set VSTAGE=vprogres
-bin\vecho /g "Studying " /f 9 gauges /f gray ...
+bin\vecho /g/s- "Studying " /f 9 gauges /f gray ...
 NASM.EXE -ISOURCE\ SOURCE\VPROGRES.ASM -fbin -O9 -o BIN\VPROGRES.COM
 if ERRORLEVEL 1 goto Error
 if not "%1" == "" goto Done
 
 :vinfo
 set VSTAGE=vinfo
-bin\vecho /g "Warming " /f 0x0a "engine" /f gray ...
+bin\vecho /g/s- "Warming " /f 0x0a "engine" /f gray ...
 NASM.EXE -ISOURCE\ SOURCE\VINFO.ASM -fbin -O9 -o BIN\VINFO.COM
 if ERRORLEVEL 1 goto Error
 if not "%1" == "" goto Done
 
 :vline
 set VSTAGE=vline
-bin\vecho /g "Reading " /f 14 map /f gray ...
+bin\vecho /g/s- "Reading " /f 14 map /f gray ...
 NASM.EXE -ISOURCE\ SOURCE\VLINE.ASM -fbin -O9 -o BIN\VLINE.COM
 if ERRORLEVEL 1 goto Error
 if not "%1" == "" goto Done
 
 :vpcspkr
 set VSTAGE=vpcspkr
-bin\vecho /g "Tuning " /f 10 radio /f gray ...
+bin\vecho /g/s- "Tuning " /f 10 radio /f gray ...
 NASM.EXE -ISOURCE\ SOURCE\VPCSPKR.ASM -fbin -O9 -o BIN\VPCSPKR.COM
 if ERRORLEVEL 1 goto Error
 if not "%1" == "" goto Done
 
 :vver
 set VSTAGE=vver
-bin\vecho /g "Referencing " /f 11 "Zen and the Art of Motorcycle Maintenance" /f gray ...
+bin\vecho /g/s- "Referencing " /f 11 "Zen and the Art of Motorcycle Maintenance" /f gray ...
 NASM.EXE -ISOURCE\ SOURCE\VVER.ASM -fbin -O9 -o BIN\VVER.COM
 if ERRORLEVEL 1 goto Error
 if not "%1" == "" goto Done
 
 :vpause
 set VSTAGE=vpause
-bin\vecho /g "Dawning " /f 9 shades /f gray ...
+bin\vecho /g/s- "Dawning " /f 9 shades /f gray ...
 NASM.EXE -ISOURCE\ SOURCE\VPAUSE.ASM -fbin -O9 -o BIN\VPAUSE.COM
 if ERRORLEVEL 1 goto Error
 if not "%1" == "" goto Done
 
 :vmode
 set VSTAGE=vmode
-bin\vecho /g "Selecting " /f 10 gear /f gray ...
+bin\vecho /g/s- "Selecting " /f 10 gear /f gray ...
 NASM.EXE -ISOURCE\ SOURCE\VMODE.ASM -fbin -O9 -o BIN\VMODE.COM
 if ERRORLEVEL 1 goto Error
 if not "%1" == "" goto Done
 
 :vask
 set VSTAGE=vask
-bin\vecho /g "Releasing " /f 11 brakes /f gray ...
+bin\vecho /g/s- "Releasing " /f 11 brakes /f gray ...
 NASM.EXE -ISOURCE\ SOURCE\VASK.ASM -fbin -O9 -o BIN\VASK.COM
 if ERRORLEVEL 1 goto Error
 if not "%1" == "" goto Done
 
 :vchkbox
 set VSTAGE=vchkbox
-bin\vecho /g "Eyeing up " /f 0x0a "traffic" /f gray ...
+bin\vecho /g/s- "Eyeing up " /f 0x0a "traffic" /f gray ...
 NASM.EXE -ISOURCE\ SOURCE\VCHKBOX.ASM -fbin -O9 -o BIN\VCHKBOX.COM
 if ERRORLEVEL 1 goto Error
 if not "%1" == "" goto Done
 
 :vfdutil
 set VSTAGE=vfdutil
-bin\vecho /g "Releasing " /f 14 clutch /f gray ...
+bin\vecho /g/s- "Releasing " /f 14 clutch /f gray ...
 NASM.EXE -ISOURCE\ SOURCE\VFDUTIL.ASM -fbin -O9 -o BIN\VFDUTIL.COM
 if ERRORLEVEL 1 goto Error
 if not "%1" == "" goto Done
 
 :vview
 set VSTAGE=vview
-bin\vecho /g "Mashing " /f 12 accelerator /f gray ...
+bin\vecho /g/s- "Mashing " /f 12 accelerator /f gray ...
 NASM.EXE -ISOURCE\ SOURCE\VVIEW.ASM -fbin -O9 -o BIN\VVIEW.COM
 if ERRORLEVEL 1 goto Error
 if not "%1" == "" goto Done
 
 set VSTAGE=
-bin\vecho /g
-bin\vecho /g /n /fLightCyan Vroom /fWhite "! "
-bin\vecho /g /n /fLightRed Vroom /fWhite "! "
-bin\vecho /g /fYellow "Lets roll" /fWhite "! " /fGray
+bin\vecho /g/s-
+bin\vecho /g/s- /n /fLightCyan Vroom /fWhite "! "
+bin\vecho /g/s- /n /fLightRed Vroom /fWhite "! "
+bin\vecho /g/s- /fYellow "Lets roll" /fWhite "! " /fGray
 
 goto Done
 :Error
