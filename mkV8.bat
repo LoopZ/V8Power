6ef72f8a266e30ac10e38c5233db82edb82d3542
Shidel <shidel@users.noreply.github.com>
2021-07-11 13:11:44 -0400

    improved DOS build script

--- mkV8.bat
+++ mkV8.bat
@@ -1,266 +1,122 @@
 @echo off
 
-set ASM=NASM.EXE
-if exist %DOSDIR%\LINKS\NASM.BAT set ASM=call NASM
-if exist %DOSDIR%\LINKS\NASM.COM set ASM=NASM.COM
+:Start
+set MKV8_SELF=%0
+if not exist %MKV8_SELF% set MKV8_SELF=%0.BAT
+if exist %MKV8_SELF% goto Begin
+set MKV8_STAGE=
+echo Unable to locate this script for compiler messages.
+goto ERROR
+
+:Begin
+set MKV8_ASM=NASM.EXE
+if exist %DOSDIR%\LINKS\NASM.BAT set MKV8_ASM=call NASM
+if exist %DOSDIR%\LINKS\NASM.COM set MKV8_ASM=NASM.COM
 
 if not "%1" == "" goto %1
-if not "%1" == "" goto VeryEnd
+if not "%1" == "" goto AllDone
 
 if "%1" == "" call mkClean.bat
 if not exist BIN\NUL mkdir BIN
 
 echo V8Power Tools for DOS!
-
-:vcls
-set VSTAGE=vcls
-echo Grabbing keys...
-%ASM% -ISOURCE\ SOURCE\%VSTAGE%.ASM -fbin -O9 -o BIN\%VSTAGE%.COM
+echo.
+call %MKV8_SELF% PreCompile VECHO
 if ERRORLEVEL 1 goto Error
-if not exist BIN\%VSTAGE%.COM goto Error
-if not "%1" == "" goto Done
-
-:vecho
-set VSTAGE=vecho
-echo Inspecting tires...
-%ASM% -ISOURCE\ SOURCE\%VSTAGE%.ASM -fbin -O9 -o BIN\%VSTAGE%.COM
+call %MKV8_SELF% PreCompile VFDUTIL
 if ERRORLEVEL 1 goto Error
-if not exist BIN\%VSTAGE%.COM goto Error
-if not "%1" == "" goto Done
-
-:vframe
-set VSTAGE=vframe
-bin\vecho /g/s- "Examining " /f lightred coolant /f Gray ...
-%ASM% -ISOURCE\ SOURCE\%VSTAGE%.ASM -fbin -O9 -o BIN\%VSTAGE%.COM
+call %MKV8_SELF% PreCompile VEACH
 if ERRORLEVEL 1 goto Error
-if not exist BIN\%VSTAGE%.COM goto Error
-if not "%1" == "" goto Done
 
-:verrlvl
-set VSTAGE=verrlvl
-bin\vecho /g/s- "Studying " /f 10 "dipstick" /f gray ...
-%ASM% -ISOURCE\ SOURCE\%VSTAGE%.ASM -fbin -O9 -o BIN\%VSTAGE%.COM
+:Loopy
+veach /S/D SOURCE\*.ASM /X %MKV8_SELF% SubCompile *
 if ERRORLEVEL 1 goto Error
-if not exist BIN\%VSTAGE%.COM goto Error
-if not "%1" == "" goto Done
+goto Done
 
-:vgotoxy
-set VSTAGE=vgotoxy
-bin\vecho /g/s- "Checking " /f yellow spark-plugs /f Gray ...
-%ASM% -ISOURCE\ SOURCE\%VSTAGE%.ASM -fbin -O9 -o BIN\%VSTAGE%.COM
-if ERRORLEVEL 1 goto Error
-if not exist BIN\%VSTAGE%.COM goto Error
-if not "%1" == "" goto Done
+:PreCompile
+set MKV8_STAGE=%2
+set MKV8_CODE=%2.ASM
+goto DoMessage
+:SubCompile
+if "%2" == "VECHO.ASM" goto VeryEnd
+if "%2" == "VFDUTIL.ASM" goto VeryEnd
+if "%2" == "VEACH.ASM" goto VeryEnd
+set /e MKV8_STAGE=BIN\VFDUTIL.COM /n %2
+if "%MKV8_STAGE" == "" goto SubCompile
+set MKV8_CODE=%2
+:DoMessage
+if exist bin\vecho.com goto FancyMessage
+echo (%MKV8_STAGE%) Cabin Fever...
+goto DoCompile
+:FancyMessage
+vecho /g /n /fDarkGray (%MKV8_STAGE%) /fGray /t %MKV8_SELF% MSG_%MKV8_STAGE% %MKV8_STAGE%
+if ERRORLEVEL 1 Echo missing message for %MKV8_STAGE%
+:DoCompile
 
-:vdelay
-set VSTAGE=vdelay
-bin\vecho /g/s- "Cleaning " /f 15 "air filter" /f gray ...
-%ASM% -ISOURCE\ SOURCE\%VSTAGE%.ASM -fbin -O9 -o BIN\%VSTAGE%.COM
-if ERRORLEVEL 1 goto Error
-if not exist BIN\%VSTAGE%.COM goto Error
-if not "%1" == "" goto Done
+if exist BIN\%MKV8_STAGE%.COM del BIN\%MKV8_STAGE%.COM>NULL
+%MKV8_ASM% -ISOURCE\ SOURCE\%MKV8_STAGE%.ASM -fbin -O9 -o BIN\%MKV8_STAGE%.COM
+if ERRORLEVEL 1 goto CompileFail
+if not exist BIN\%MKV8_STAGE%.COM CompileFail
 
-:vmath
-set VSTAGE=vmath
-bin\vecho /g/s- /n "Testing "
-bin\vecho /g/s- /f 13 belts /f gray ...
-%ASM% -ISOURCE\ SOURCE\%VSTAGE%.ASM -fbin -O9 -o BIN\%VSTAGE%.COM
-if ERRORLEVEL 1 goto Error
-if not exist BIN\%VSTAGE%.COM goto Error
-if not "%1" == "" goto Done
+:CompileOk
+vecho /g /t %MKV8_SELF% MSG_SUCCESS
+goto VeryEnd
+:CompileFail
+vecho /g /t %MKV8_SELF% MSG_FAIL
+verrlvl 1>NULL
+goto VeryEnd
 
-:vstr
-set VSTAGE=vstr
-bin\vecho /g/s- "Turning " /f 10 "the key" /f gray ...
-%ASM% -ISOURCE\ SOURCE\%VSTAGE%.ASM -fbin -O9 -o BIN\%VSTAGE%.COM
-if ERRORLEVEL 1 goto Error
-if not exist BIN\%VSTAGE%.COM goto Error
-if not "%1" == "" goto Done
+### Displayed messages
 
-:vchoice
-set VSTAGE=vchoice
-bin\vecho /g/s- "Verifying " /f 12 "timing" /f gray ...
-%ASM% -ISOURCE\ SOURCE\%VSTAGE%.ASM -fbin -O9 -o BIN\%VSTAGE%.COM
-if ERRORLEVEL 1 goto Error
-if not exist BIN\%VSTAGE%.COM goto Error
-if not "%1" == "" goto Done
+MSG_SUCCESS=/f LightGreen /c32 Success /f Gray
+MSG_FAIL=/f LightRed /c32 Failed! /f Gray
+MSG_VECHO=Grabbing /fYellow keys /fGray /s- ...
+MSG_VFDUTIL=Inspecting /fWhite tires /fGray /s- ...
+MSG_VEACH=Examining /f lightred coolant /f Gray /s- ...
+MSG_VASK=Studying /f 10 dipstick /f gray /s- ...
+MSG_VCHKBOX=Checking /f yellow spark-plugs /f Gray /s- ...
+MSG_VCHOICE=Cleaning /f 15 air filter /f gray /s- ...
+MSG_VCLS=Testing /f 13 belts /f gray /s- ...
+MSG_VCURSOR=Turning /f 10 the key /f gray /s- ...
+MSG_VDELAY=Verifying /f 12 timing /f gray /s- ...
+MSG_VDELETE=Adjusting /f 13 carburetor /f gray /s- ...
+MSG_VERRLVL=Examining /f 9 gauges /f gray /s- ...
+MSG_VFONT=Warming /f 0x0a engine /f gray /s- ...
+MSG_VFRAME=Opening /f 14 map /f gray /s- ...
+MSG_VGOTOXY=Planing /f10 waypoints /f gray /s- ...
+MSG_VHDUTIL=Studying /f 12 route /f gray /s- ...
+MSG_VINFO=Tuning /f 10 radio /f gray /s- ...
+MSG_VINSERT=Referencing /f 11 Zen and the Art of Motorcycle Maintenance /f gray /s- ...
+MSG_VLINE=Dawning /f 9 shades /f gray /s- ...
+MSG_VMATH=Selecting /f 10 gear /f gray /s- ...
+MSG_VMODE=Releasing /f 11 brakes /f gray /s- ...
+MSG_VPAUSE=Reving /f 13 engine /f gray /s- ...
+MSG_VPCSPKR=Spotting /f 10 gap /f gray /s- ...
+MSG_VPROGRES=Engaging /f 12 signal /f gray /s- ...
+MSG_VREADKEY=Releasing /f 14 clutch /f gray /s- ...
+MSG_VSTR=Eyeing /f 0x0a surroundings /f gray /s- ...
+MSG_VVER=Monitoring /f 5 traffic /f gray /s- ...
+MSG_VVIEW=Mashing /f 12 accelerator /f gray /s- ...
 
-:vcursor
-set VSTAGE=vcursor
-bin\vecho /g/s- /n "Adjusting "
-bin\vecho /g/s- /f 13 carburetor /f gray ...
-%ASM% -ISOURCE\ SOURCE\%VSTAGE%.ASM -fbin -O9 -o BIN\%VSTAGE%.COM
-if ERRORLEVEL 1 goto Error
-if not exist BIN\%VSTAGE%.COM goto Error
-if not "%1" == "" goto Done
+:Error
+if "%MKV8_STAGE%" == "" echo Script failure.
+if not "%MKV8_STAGE%" == "" echo Error creating utility %MKV8_STAGE%.com.
+goto AllDone
 
-:vprogres
-set VSTAGE=vprogres
-bin\vecho /g/s- "Examining " /f 9 gauges /f gray ...
-%ASM% -ISOURCE\ SOURCE\%VSTAGE%.ASM -fbin -O9 -o BIN\%VSTAGE%.COM
-if ERRORLEVEL 1 goto Error
-if not exist BIN\%VSTAGE%.COM goto Error
-if not "%1" == "" goto Done
-
-:vinfo
-set VSTAGE=vinfo
-bin\vecho /g/s- "Warming " /f 0x0a "engine" /f gray ...
-%ASM% -ISOURCE\ SOURCE\%VSTAGE%.ASM -fbin -O9 -o BIN\%VSTAGE%.COM
-if ERRORLEVEL 1 goto Error
-if not exist BIN\%VSTAGE%.COM goto Error
-if not "%1" == "" goto Done
-
-:vline
-set VSTAGE=vline
-bin\vecho /g/s- "Opening " /f 14 map /f gray ...
-%ASM% -ISOURCE\ SOURCE\%VSTAGE%.ASM -fbin -O9 -o BIN\%VSTAGE%.COM
-if ERRORLEVEL 1 goto Error
-if not exist BIN\%VSTAGE%.COM goto Error
-if not "%1" == "" goto Done
-
-:veach
-set VSTAGE=veach
-bin\vecho /g Planing /f10 waypoints /s-/f gray ...
-%ASM% -ISOURCE\ SOURCE\%VSTAGE%.ASM -fbin -O9 -o BIN\%VSTAGE%.COM
-if ERRORLEVEL 1 goto Error
-if not exist BIN\%VSTAGE%.COM goto Error
-if not "%1" == "" goto Done
-
-:vwhat
-rem set VSTAGE=vwhat
-rem bin\vecho /g/s- /n "Studying "
-rem bin\vecho /g/s- /f 12 route /f gray ...
-rem %ASM% -ISOURCE\ SOURCE\%VSTAGE%.ASM -fbin -O9 -o BIN\%VSTAGE%.COM
-rem if ERRORLEVEL 1 goto Error
-rem if not exist BIN\%VSTAGE%.COM goto Error
-rem if not "%1" == "" goto Done
-
-:vpcspkr
-set VSTAGE=vpcspkr
-bin\vecho /g/s- "Tuning " /f 10 radio /f gray ...
-%ASM% -ISOURCE\ SOURCE\%VSTAGE%.ASM -fbin -O9 -o BIN\%VSTAGE%.COM
-if ERRORLEVEL 1 goto Error
-if not exist BIN\%VSTAGE%.COM goto Error
-if not "%1" == "" goto Done
-
-:vver
-set VSTAGE=vver
-bin\vecho /g/s- "Referencing " /f 11 "Zen and the Art of Motorcycle Maintenance" /f gray ...
-%ASM% -ISOURCE\ SOURCE\%VSTAGE%.ASM -fbin -O9 -o BIN\%VSTAGE%.COM
-if ERRORLEVEL 1 goto Error
-if not exist BIN\%VSTAGE%.COM goto Error
-if not "%1" == "" goto Done
-
-:vpause
-set VSTAGE=vpause
-bin\vecho /g/s- "Dawning " /f 9 shades /f gray ...
-%ASM% -ISOURCE\ SOURCE\%VSTAGE%.ASM -fbin -O9 -o BIN\%VSTAGE%.COM
-if ERRORLEVEL 1 goto Error
-if not exist BIN\%VSTAGE%.COM goto Error
-if not "%1" == "" goto Done
-
-:vmode
-set VSTAGE=vmode
-bin\vecho /g/s- "Selecting " /f 10 gear /f gray ...
-%ASM% -ISOURCE\ SOURCE\%VSTAGE%.ASM -fbin -O9 -o BIN\%VSTAGE%.COM
-if ERRORLEVEL 1 goto Error
-if not exist BIN\%VSTAGE%.COM goto Error
-if not "%1" == "" goto Done
-
-:vask
-set VSTAGE=vask
-bin\vecho /g/s- "Releasing " /f 11 brakes /f gray ...
-%ASM% -ISOURCE\ SOURCE\%VSTAGE%.ASM -fbin -O9 -o BIN\%VSTAGE%.COM
-if ERRORLEVEL 1 goto Error
-if not exist BIN\%VSTAGE%.COM goto Error
-if not "%1" == "" goto Done
-
-:vinsert
-set VSTAGE=vinsert
-bin\vecho /g/s- "Reving " /f 13 engine /f gray ...
-%ASM% -ISOURCE\ SOURCE\%VSTAGE%.ASM -fbin -O9 -o BIN\%VSTAGE%.COM
-if ERRORLEVEL 1 goto Error
-if not exist BIN\%VSTAGE%.COM goto Error
-if not "%1" == "" goto Done
-
-:vfont
-set VSTAGE=vfont
-bin\vecho /g/s- "Spotting " /f 10 gap /f gray ...
-%ASM% -ISOURCE\ SOURCE\%VSTAGE%.ASM -fbin -O9 -o BIN\%VSTAGE%.COM
-if ERRORLEVEL 1 goto Error
-if not exist BIN\%VSTAGE%.COM goto Error
-if not "%1" == "" goto Done
-
-:vdelete
-set VSTAGE=vdelete
-bin\vecho /g/s- "Engaging " /f 12 signal /f gray ...
-%ASM% -ISOURCE\ SOURCE\%VSTAGE%.ASM -fbin -O9 -o BIN\%VSTAGE%.COM
-if ERRORLEVEL 1 goto Error
-if not exist BIN\%VSTAGE%.COM goto Error
-if not "%1" == "" goto Done
-
-:vfdutil
-set VSTAGE=vfdutil
-bin\vecho /g/s- "Releasing " /f 14 clutch /f gray ...
-%ASM% -ISOURCE\ SOURCE\%VSTAGE%.ASM -fbin -O9 -o BIN\%VSTAGE%.COM
-if ERRORLEVEL 1 goto Error
-if not exist BIN\%VSTAGE%.COM goto Error
-if not "%1" == "" goto Done
-
-goto Skippy
-:vchkbox
-set VSTAGE=vchkbox
-bin\vecho /g/s- "Eyeing " /f 0x0a "surroundings" /f gray ...
-%ASM% -ISOURCE\ SOURCE\%VSTAGE%.ASM -fbin -O9 -o BIN\%VSTAGE%.COM
-if ERRORLEVEL 1 goto Error
-if not exist BIN\%VSTAGE%.COM goto Error
-if not "%1" == "" goto Done
-
-:vreadkey
-set VSTAGE=vreadkey
-bin\vecho /g/s- "Monitoring " /f 5 traffic /f gray ...
-%ASM% -ISOURCE\ SOURCE\%VSTAGE%.ASM -fbin -O9 -o BIN\%VSTAGE%.COM
-if ERRORLEVEL 1 goto Error
-if not exist BIN\%VSTAGE%.COM goto Error
-if not "%1" == "" goto Done
-
-:vview
-set VSTAGE=vview
-bin\vecho /g/s- "Mashing " /f 12 accelerator /f gray ...
-%ASM% -ISOURCE\ SOURCE\%VSTAGE%.ASM -fbin -O9 -o BIN\%VSTAGE%.COM
-if ERRORLEVEL 1 goto Error
-if not exist BIN\%VSTAGE%.COM goto Error
-if not "%1" == "" goto Done
-
-:Skippy
-set VSTAGE=
+:Done
 bin\vecho /g/s-
 bin\vecho /g/s- /n /fLightCyan Vroom /fWhite "! "
 bin\vecho /g/s- /n /fLightRed Vroom /fWhite "! "
 bin\vecho /g/s- /fYellow "Lets roll" /fWhite "! " /fGray
-
-goto Done
-:Error
-echo Error creating utility %VSTAGE%.com.
-goto VeryEnd
-
-:Done
-if not "%1" == "" echo.
-
-if not "%1" == "" dir BIN\%1.COM | grep -i "%1"
-if "%2" == "" goto NoMore
-%0 %2 %3 %4 %5 %6 %7 %8 %9
-
-:NoMore
-
-rem if not "%1" == "" goto VeryEnd
-:vdocs
-if not exist BIN goto VeryEnd
+if not exist BIN\NULL goto AllDone
 copy LICENSE BIN >NUL
 if exist README.TXT copy README.TXT BIN\V8POWER.TXT >NUL
 if not exist README.TXT copy README.md BIN\V8POWER.md >NUL
 
-:VeryEnd
-set VSTAGE=
-set ASM=
+:AllDone
+set MKV8_SELF=
+set MKV8_STAGE=
+set MKV8_CODE=
+set MKV8_ASM=
 
+:VeryEnd
